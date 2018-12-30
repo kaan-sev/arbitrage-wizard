d74bd8f8b0a6120aff7e6fb7beb6fa947c421628
Kaan <33149606+kaan-sev@users.noreply.github.com>
2018-12-31 01:04:35 +1100

    Add files via upload

--- ArbitrageWizard/MainWindow.xaml.cs
+++ ArbitrageWizard/MainWindow.xaml.cs
@@ -23,6 +23,7 @@ namespace ArbitrageWizard
         public MainWindow()
         {
             InitializeComponent();
+            mbrDataGrid.ItemsSource = LoadCollectionData();  
         }
 
         private void TextBox_PreviewKeyDown(object sender, KeyEventArgs e)
@@ -63,15 +64,73 @@ namespace ArbitrageWizard
         {
             doubleChecker(layOdds, e);
         }
+        private void backOdds2_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            doubleChecker(backOdds2, e);
+        }
+        private void backOutlay2_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            doubleChecker(backOutlay2, e);
+        }
+
+        private void layOdds2_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            doubleChecker(layOdds2, e);
+        }
+        private void weightingDoubleChecker(TextBox box, TextCompositionEventArgs e)
+        {
+            int cursorPos = box.CaretIndex;
+            string nextText;
+            if (cursorPos > 0)
+            {
+                nextText = box.Text.Substring(0, cursorPos) + e.Text + box.Text.Substring(cursorPos);
+            }
+            else
+            {
+                nextText = box.Text + e.Text;
+            }
+            double testVal;
+            if (!Double.TryParse(nextText, out testVal))
+            {
+                e.Handled = true;
+            }
+            else if (testVal > 1)
+            {
+                e.Handled = true;
+            }
+            else
+            {
+                weighting2.Text = (1 - testVal).ToString();
+            }
+        }
+        private void weighting1_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            weightingDoubleChecker(weighting1, e);
+        }
 
         private void Calculate_Click(object sender, RoutedEventArgs e)
         {
             double parsedLayOdds;
             double parsedBackOutlay;
             double parsedBackOdds;
-            if (Double.TryParse(layOdds.Text, out parsedLayOdds) && Double.TryParse(backOdds.Text, out parsedBackOdds) && Double.TryParse(backOutlay.Text, out parsedBackOutlay))
+            double parsedDiscountRate = discountRate.Value;
+            double parsedMBR = mbRate.Value;
+            if (Double.TryParse(layOdds.Text, out parsedLayOdds) && Double.TryParse(backOdds.Text, out parsedBackOdds) && 
+                Double.TryParse(backOutlay.Text, out parsedBackOutlay))
             {
-                double impliedOddsNum = (1 + (1 / (parsedLayOdds - 1)));
+                double commission = 0;
+                if (isMBR.IsChecked ?? false) 
+                {
+                    if (isDiscountRate.IsChecked ?? false)
+                    {
+                        commission = ((100-parsedDiscountRate)*parsedMBR)/10000;
+                    }
+                    else
+                    {
+                        commission = parsedMBR/100;
+                    }
+                }
+                double impliedOddsNum = (1 + (1 - commission)*(1 / ((parsedLayOdds - 1))));
                 double maxWin = (parsedBackOdds * parsedBackOutlay);
                 double liabilityNum = maxWin / impliedOddsNum;
                 double backersStakeNum = maxWin - liabilityNum;
@@ -92,29 +151,133 @@ namespace ArbitrageWizard
                 profit.Content = Math.Round(maxProfit, 2);
             }
         }
-        private void about_Click(object sender, RoutedEventArgs e)
-        {
 
-        }
-        private void quit_Click(object sender, RoutedEventArgs e)
+        private void CalculateCustom_Click(object sender, RoutedEventArgs e)
         {
-            Application.Current.Shutdown();
+            double parsedLayOdds;
+            double parsedBackOutlay;
+            double parsedBackOdds;
+            double parsedDiscountRate = discountRate.Value;
+            double parsedMBR = mbRate.Value;
+            double parsedBackWeighting;
+            double parsedLayWeighting;
+            if (Double.TryParse(layOdds2.Text, out parsedLayOdds) && Double.TryParse(backOdds2.Text, out parsedBackOdds) &&
+                Double.TryParse(backOutlay2.Text, out parsedBackOutlay) && Double.TryParse(weighting1.Text, out parsedBackWeighting) &&
+                Double.TryParse(weighting2.Text, out parsedLayWeighting))
+            {
+                double commission = 0;
+                if (isMBR2.IsChecked ?? false)
+                {
+                    if (isDiscountRate2.IsChecked ?? false)
+                    {
+                        commission = ((100 - parsedDiscountRate) * parsedMBR) / 10000;
+                    }
+                    else
+                    {
+                        commission = parsedMBR / 100;
+                    }
+                }
+                double impliedOddsNum = (1 + (1 - commission) * (1 / ((parsedLayOdds - 1))));
+                double maxWin = (parsedBackOdds * parsedBackOutlay);
+                double liabilityNum = maxWin * (1-parsedBackWeighting) / ((parsedBackWeighting)*(impliedOddsNum-2)+1);
+                double backersStakeNum = maxWin - liabilityNum;
+                double totalOutlayNum = liabilityNum + parsedBackOutlay;
+                double profitBackNum = maxWin - totalOutlayNum;
+                double profitLayNum = liabilityNum * impliedOddsNum - totalOutlayNum;
+                if (isBonusBet2.IsChecked ?? false)
+                {
+                    maxWin = maxWin - parsedBackOutlay;
+                    liabilityNum = maxWin * (1 - parsedBackWeighting) / ((parsedBackWeighting) * (impliedOddsNum - 2) + 1);
+                    backersStakeNum = maxWin - liabilityNum;
+                    totalOutlayNum = liabilityNum;
+                    profitBackNum = maxWin - totalOutlayNum;
+                    profitLayNum = liabilityNum * impliedOddsNum - totalOutlayNum;
+                }
+                impliedOdds2.Content = Math.Round(impliedOddsNum, 2);
+                backersStake2.Content = Math.Round(backersStakeNum, 2);
+                liability2.Content = Math.Round(liabilityNum, 2);
+                totalOutlay2.Content = Math.Round(totalOutlayNum, 2);
+                profitBack.Content = Math.Round(profitBackNum, 2);
+                profitLay.Content = Math.Round(profitLayNum, 2);
+            }
         }
-        private void help_Click(object sender, RoutedEventArgs e)
+        private List<Region> LoadCollectionData()
         {
+            List<Region> regions = new List<Region>();
+            regions.Add(new Region()
+            {
+                Location = "NSW",
+                Thoroughbred = "10%",
+                Harness = "8%",
+                Greyhound = "6%",
+                Sports = "5%"
+            });
+            regions.Add(new Region()
+            {
+                Location = "VIC",
+                Thoroughbred = "6%",
+                Harness = "6%",
+                Greyhound = "7%",
+                Sports = "5%"
+            });
+            regions.Add(new Region()
+            {
+                Location = "QLD",
+                Thoroughbred = "6%",
+                Harness = "6%",
+                Greyhound = "6%",
+                Sports = "5%"
+            });
+            regions.Add(new Region()
+            {
+                Location = "SA",
+                Thoroughbred = "6%",
+                Harness = "8%",
+                Greyhound = "6%",
+                Sports = "5%"
+            });
+            regions.Add(new Region()
+            {
+                Location = "WA",
+                Thoroughbred = "8%",
+                Harness = "8%",
+                Greyhound = "8%",
+                Sports = "5%"
+            });
+            regions.Add(new Region()
+            {
+                Location = "NT",
+                Thoroughbred = "6%",
+                Harness = "N/A",
+                Greyhound = "6%",
+                Sports = "5%"
+            });
+            regions.Add(new Region()
+            {
+                Location = "ACT",
+                Thoroughbred = "7%",
+                Harness = "8%",
+                Greyhound = "N/A",
+                Sports = "5%"
+            });
+            regions.Add(new Region()
+            {
+                Location = "NZL",
+                Thoroughbred = "6%",
+                Harness = "6%",
+                Greyhound = "6%",
+                Sports = "5%"
+            });
+            regions.Add(new Region()
+            {
+                Location = "INTL",
+                Thoroughbred = "5%",
+                Harness = "5%",
+                Greyhound = "5%",
+                Sports = "5%"
+            });
 
-        }
-        private void commissionRates_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-        private void discountRate_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-        private void advSettings_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
+            return regions;
+        }  
     }
 }
---
+++ ArbitrageWizard/Region.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbitrageWizard
+{
+    class Region
+    {
+        public string Location { get; set; }
+        public string Thoroughbred { get; set; }
+        public string Harness { get; set; }
+        public string Greyhound { get; set; }
+        public string Sports { get; set; }
+
+
+    }
+}
